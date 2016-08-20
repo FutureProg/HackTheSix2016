a4134102081d6b97db534695ada2b3e3da3a0fe2
Nickolas A. Morrison <nickmorrison09@gmail.com>
2016-08-20 15:21:36 -0400

    made changes to database.php

--- web/database.php
+++ web/database.php
@@ -3,8 +3,16 @@
 class ImageDB extends SQLite3{
 
   function __construct(){
-
+    $this->open("images.db");
   }
   
 }
+
+$db = new ImageDB();
+if(!$db){
+  echo $db->lastErrorMsg();
+} else {
+  echo "Opened database successfully\n";
+}
+
 
