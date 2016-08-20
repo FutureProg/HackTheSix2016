8b8d06e191a563c26c74751bd720cdcdf327b1f8
Nickolas A. Morrison <nickmorrison09@gmail.com>
2016-08-20 17:07:58 -0400

    Changes made to db

--- web/database.php
+++ web/database.php
@@ -5,6 +5,31 @@ class ImageDB extends SQLite3{
   function __construct(){
     $this->open("images.db");
   }
+
+  function addImage($name,$givenname,$rating,$path){
+    $query =<<<EOF
+      INSERT INTO Images (name,given_name,rating,path)
+      VALUES ($name,$givenname,$rating,$path);
+    EOF;
+    $re = $this->exec($query);
+    if(!$re){
+      echo $this->lastErrorMsg();
+    } else {
+      echo "OKAY";
+    }
+  }
+
+  function findImages($name){
+    $query =<<<EOF
+      SELECT * FROM Images WHERE name=$name;
+    EOF;
+    $re = $this->exec($query);
+    if(!$re){
+      echo $this->lastErrorMsg();
+    }else{
+      echo "OKAY";
+    }
+  }
   
 }
 
