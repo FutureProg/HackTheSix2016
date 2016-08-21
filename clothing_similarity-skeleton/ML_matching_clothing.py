b160706e94aeb9ef42d22c41212f0ce2074c0458
FutureProg <nickmorrison09@gmail.com>
2016-08-21 03:51:25 +0000

    fixed

--- clothing_similarity-skeleton/ML_matching_clothing.py
+++ clothing_similarity-skeleton/ML_matching_clothing.py
@@ -22,7 +22,7 @@ n_img_displayed = 10
 def make_paths_list():
     d = []
     i = 0
-    for root, dirs, files in os.walk("images"):
+    for root, dirs, files in os.walk("../clothing_similarity-skeleton/images"):
         for image in files:
             if image.endswith(".jpg"):
                 d.append(os.path.join(root, image))
